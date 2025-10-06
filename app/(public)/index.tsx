f199f9dfe15469d8994c0e5c72b0ca6a2f936cc2
deepak <deepakkumardash313@gmail.com>
2025-10-07 03:44:55 +0530

    Add : login ui and added oauth

--- app/(public)/index.tsx
+++ app/(public)/index.tsx
@@ -1,15 +1,142 @@
-import { Text, View } from "react-native";
+import { Colors } from "@/constants/Colors";
+import { useSSO } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function Index() {
+  const { startSSOFlow } = useSSO();
+
+  const handleFacebookLogin = async () => {
+    try {
+      const { setActive, createdSessionId } = await startSSOFlow({
+        strategy: "oauth_facebook",
+      });
+
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+      }
+    } catch (error) {
+      console.error("OAuth error", error);
+    }
+  };
+
+  const handleGoogleLogin = async () => {
+    try {
+      const { setActive, createdSessionId } = await startSSOFlow({
+        strategy: "oauth_google",
+      });
+
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+      }
+    } catch (error) {
+      console.error("OAuth error", error);
+    }
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Threads App.</Text>
+    <View style={styles.container}>
+      <Image
+        style={styles.loginImage}
+        source={require("@/assets/images/login.png")}
+      />
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.title}>How would you like to use threads?</Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.loginButton}
+            onPress={handleFacebookLogin}
+          >
+            <View style={styles.loginButtonContent}>
+              <Image
+                source={require("@/assets/images/facebook.png")}
+                style={styles.loginButtonIcon}
+              />
+              <Text style={styles.loginButtonText}>Continue with Facebook</Text>
+              <Ionicons
+                name="chevron-forward"
+                size={24}
+                color={Colors.border}
+              />
+            </View>
+            <Text style={styles.loginButtonSubtitle}>
+              Log in or create a threads profile with your Facebook account.
+              With a profile, you can post, interact and get personalised
+              recommendations.
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.loginButton}
+            onPress={handleGoogleLogin}
+          >
+            <View style={styles.loginButtonContent}>
+              <Ionicons name="logo-google" size={24} />
+              <Text style={styles.loginButtonText}>Continue with Google</Text>
+              <Ionicons
+                name="chevron-forward"
+                size={24}
+                color={Colors.border}
+              />
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 20,
+    alignItems: "center",
+    backgroundColor: Colors.background,
+  },
+  loginImage: {
+    width: "100%",
+    height: 350,
+    resizeMode: "cover",
+  },
+  title: {
+    fontSize: 17,
+    fontFamily: "DMSans_700Bold",
+  },
+  loginButton: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 8,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: Colors.border,
+  },
+  buttonContainer: {
+    gap: 20,
+    marginHorizontal: 20,
+  },
+  loginButtonContent: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  loginButtonIcon: {
+    width: 50,
+    height: 50,
+  },
+  loginButtonText: {
+    fontSize: 15,
+    fontFamily: "DMSans_500Medium",
+    flex: 1,
+  },
+  loginButtonSubtitle: {
+    fontSize: 12,
+    fontFamily: "DMSans_400Regular",
+    color: "#acacac",
+    marginTop: 5,
+  },
+});
