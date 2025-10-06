fe99750f440090ca9ca5f218ddfcd3f53564406a
deepak <deepakkumardash313@gmail.com>
2025-10-06 22:24:31 +0530

    Add : intialize fonts and add clerk auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,48 @@
+import { tokenCache } from "@/utils/tokenCache";
+import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
+import {
+  DMSans_400Regular,
+  DMSans_500Medium,
+  DMSans_700Bold,
+  useFonts,
+} from "@expo-google-fonts/dm-sans";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+const clerkPublishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+SplashScreen.preventAutoHideAsync();
+
+const InitialLayout = () => {
+  const [fonstLoaded] = useFonts({
+    DMSans_400Regular,
+    DMSans_500Medium,
+    DMSans_700Bold,
+  });
+
+  useEffect(() => {
+    if (fonstLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fonstLoaded]);
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+};
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider
+      publishableKey={clerkPublishableKey!}
+      tokenCache={tokenCache}
+    >
+      <ClerkLoaded>
+        <InitialLayout />
+      </ClerkLoaded>
+    </ClerkProvider>
+  );
 }
